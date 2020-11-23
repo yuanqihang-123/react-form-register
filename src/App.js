04977dd43f76d559244cc0276c1eb95c22c3de93
yuan <43234088+yuanqihang-123@users.noreply.github.com>
2020-11-23 23:24:56 +0800

    complete validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import './styles.css';
-import { ERRORS } from './constants';
+import React from "react";
+import "./styles.css";
+import { ERRORS } from "./constants";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      showUsername: false,
+      showPassword: false
+    };
+  }
+
+  checkUsername(event) {
+    if (!ERRORS.username.regex.test(event.target.value)) {
+      this.setState({
+        showUsername: true
+      });
+    } else {
+      this.setState({
+        showUsername: false
+      });
+    }
+  }
+
+  checkPassword(event) {
+    if (!ERRORS.password.regex.test(event.target.value)) {
+      this.setState({
+        showPassword: true
+      });
+    } else {
+      this.setState({
+        showPassword: false
+      });
+    }
   }
 
   render() {
@@ -16,26 +44,32 @@ export default class App extends React.Component {
             width="100px"
           />
         </div>
-        <form noValidate>
-          <label for="username">
+        <form action="http://www.baidu.com">
+          <label htmlFor="username">
             <span className="label-star">*</span> Username:
             <input
               id="username"
               name="username"
               placeholder="6~10 characters"
               type="text"
-              noValidate
+              onChange={(event) => this.checkUsername(event)}
             />
+            {this.state.showUsername && (
+              <span className="label-star">{ERRORS.username.message}</span>
+            )}
           </label>
-          <label for="password">
+          <label htmlFor="password">
             <span className="label-star">*</span> Password:
             <input
               id="password"
               name="password"
               placeholder="6~10 numbers"
               type="password"
-              noValidate
+              onChange={(event) => this.checkPassword(event)}
             />
+            {this.state.showPassword && (
+              <span className="label-star">{ERRORS.password.message}</span>
+            )}
           </label>
           <button type="submit">Register</button>
         </form>
